58ff0df40a1e46d5a1c81c59bb74c3d8b21b1719
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-05-20 19:48:05 +0530

    feat(single_feature): implmement dynamic learning rate and convergence threshold for gradient decent

--- machine_learning/supervised/regression/linear_regression/single_feature/cpp/implementations/lregression.cpp
+++ machine_learning/supervised/regression/linear_regression/single_feature/cpp/implementations/lregression.cpp
@@ -6,7 +6,7 @@
 L_Regression::L_Regression() {
   this->w = 0;
   this->b = 0;
-  this->alpha = 0.001;
+  this->alpha = 0.01;
 }
 double L_Regression::y_predict(double x) const {
   return (x * this->w) + this->b;
@@ -30,6 +30,10 @@ double L_Regression::cost(const std::vector<Datapoint> &dataset) {
 
 void L_Regression::fit(const std::vector<Datapoint> &dataset,
                        const int epochs) {
+
+  const double threshold = 0.001;
+  double prev_cost = 0.0;
+
   for (int i = 0; i < epochs; i++) {
     double dw = 0, db = 0;
     for (auto &data : dataset) {
@@ -41,10 +45,22 @@ void L_Regression::fit(const std::vector<Datapoint> &dataset,
     dw /= dataset.size();
     db /= dataset.size();
 
-    const double w_new = this->w - (this->alpha * dw);
-    const double b_new = this->b - (this->alpha * db);
+    const double dynamic_alpha =
+        this->alpha * (((static_cast<double>(epochs - i) / epochs) * 13) + 1);
+    const double w_new = this->w - (dynamic_alpha * dw);
+    const double b_new = this->b - (dynamic_alpha * db);
 
     this->w = w_new;
     this->b = b_new;
+
+    if ((i + 2) % 100 == 0) {
+      prev_cost = this->cost(dataset);
+    } else if ((i + 1) % 100 == 0) {
+      const double next_cost = this->cost(dataset);
+      if (prev_cost - next_cost <= threshold) {
+        std::cout << "\nBreak at " << i << "th Iterations\n";
+        break;
+      }
+    }
   }
 }
